7bb3903654ea12561efa7a72b5daf091edd77efc
Koen Gilissen <gilissenkoen@gmail.com>
2013-09-30 20:38:02 +0200

    Added XY conversion in output file

--- src/com/Square9/AndroidMapsV2Test/SaveToFile.java
+++ src/com/Square9/AndroidMapsV2Test/SaveToFile.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.text.format.Time;
+import com.google.android.gms.internal.bj;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Iterator;
@@ -93,6 +94,8 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
         final String USER_COMMENT = "User Comment:";
         final String LATITUDE = "Latitude:";
         final String LONGITUDE = "Longitude:";
+        final String MPX = "X:";
+        final String MPY = "Y:";
         final String PHOTO = "Photo:";
         final String CLOSE_MEASUREMENTPOINT_TAG = "</Measurement Point>";
         final String OPEN_LINE_TAG = "<Line>";
@@ -100,6 +103,10 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
         final String LINE_LON1 = "Longitude 1:";
         final String LINE_LAT2 = "Latitude 2:";
         final String LINE_LON2 = "Longitude 2:";
+        final String LINE_X1 = "X1:";
+        final String LINE_Y1 = "Y1:";
+        final String LINE_X2 = "X2:";
+        final String LINE_Y2 = "Y2:";
         final String CLOSE_LINE_TAG = "</Line>";
         final String OPEN_ARC_TAG = "<Arc>";
         final String ARC_LAT1 = "Latitude 1:";
@@ -108,6 +115,12 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
         final String ARC_LON2 = "Longitude 2:";
         final String ARC_LAT3 = "Latitude 3:";
         final String ARC_LON3 = "Longitude 3:";
+        final String ARC_X1 = "X1:";
+        final String ARC_Y1 = "Y1:";
+        final String ARC_X2 = "X2:";
+        final String ARC_Y2 = "Y2:";
+        final String ARC_X3 = "X3:";
+        final String ARC_Y3 = "Y3:";
         final String CLOSE_ARC_TAG = "</ARC>";
 
         StringBuilder result = new StringBuilder();
@@ -131,10 +144,13 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
             // [3] for each measurement point in this layer:
             for(int pointIndex = 0; pointIndex < layer.getNumberOfMeasurementPoints(); pointIndex++)
             {
+                double mpXY[] = conversionLatLngToXYNed(layer.getMeasurementPointByIndex(pointIndex).getPosition());
                 result.append("\t" + OPEN_MEASUREMENTPOINT_TAG + "\n");
                 result.append("\t" + USER_COMMENT + layer.getMeasurementPointByIndex(pointIndex).getComment() + "\n");
                 result.append("\t" + LATITUDE + layer.getMeasurementPointByIndex(pointIndex).getPosition().latitude + "\n");
                 result.append("\t" + LONGITUDE + layer.getMeasurementPointByIndex(pointIndex).getPosition().longitude + "\n");
+                result.append("\t" + MPX + Double.toString(mpXY[0]) + "\n");
+                result.append("\t" + MPY + Double.toString(mpXY[1]) + "\n");
                 result.append("\t" + PHOTO +  layer.getMeasurementPointByIndex(pointIndex).getPhotoFilePath() + "\n");
                 result.append("\t" + CLOSE_MEASUREMENTPOINT_TAG +"\n");
             }
@@ -143,11 +159,17 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
             while(lineIterator.hasNext())
             {
                 MeasurementLine line = lineIterator.next();
+                double lineP1[] = conversionLatLngToXYNed(line.getPointOne());
+                double lineP2[] = conversionLatLngToXYNed(line.getPointTwo());
                 result.append("\t" + OPEN_LINE_TAG + "\n");
                 result.append("\t" + LINE_LAT1 + line.getPointOne().latitude  + "\n");
                 result.append("\t" + LINE_LON1 + line.getPointOne().longitude  + "\n");
                 result.append("\t" + LINE_LAT2 + line.getPointTwo().latitude  + "\n");
                 result.append("\t" + LINE_LON2 + line.getPointTwo().longitude  + "\n");
+                result.append("\t" + LINE_X1 + lineP1[0] + "\n");
+                result.append("\t" + LINE_Y1 + lineP1[1] + "\n");
+                result.append("\t" + LINE_X2 + lineP2[0] + "\n");
+                result.append("\t" + LINE_Y2 + lineP2[1] + "\n");
                 result.append("\t" + CLOSE_LINE_TAG + "\n");
             }
             // [5] for each arc in this layer
@@ -158,6 +180,9 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
                 LatLng mPos1 = arc.getMeasurementPositions().get(0);
                 LatLng mPos2 = arc.getMeasurementPositions().get(1);
                 LatLng mPos3 = arc.getMeasurementPositions().get(2);
+                double arcP1[] = conversionLatLngToXYNed(mPos1);
+                double arcP2[] = conversionLatLngToXYNed(mPos2);
+                double arcP3[] = conversionLatLngToXYNed(mPos3);
                 result.append("\t" + OPEN_ARC_TAG + "\n");
                 result.append("\t" + ARC_LAT1 + mPos1.latitude +"\n");
                 result.append("\t" + ARC_LON1 + mPos1.longitude +"\n");
@@ -165,6 +190,12 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
                 result.append("\t" + ARC_LON2 + mPos2.longitude +"\n");
                 result.append("\t" + ARC_LAT3 + mPos3.latitude +"\n");
                 result.append("\t" + ARC_LON3 + mPos3.longitude +"\n");
+                result.append("\t" + ARC_X1 + arcP1[0] +"\n");
+                result.append("\t" + ARC_Y1 + arcP1[1] +"\n");
+                result.append("\t" + ARC_X2 + arcP2[0] +"\n");
+                result.append("\t" + ARC_Y2 + arcP2[1] +"\n");
+                result.append("\t" + ARC_X3 + arcP3[0] +"\n");
+                result.append("\t" + ARC_Y3 + arcP3[1] +"\n");
                 result.append("\t" + CLOSE_ARC_TAG + "\n");
 
             }
@@ -173,5 +204,35 @@ public class SaveToFile extends AsyncTask<LayerManager, Integer, Integer>
         return result;
     }
 
+
+    private double[] conversionLatLngToXYNed(LatLng posToConvert)
+    {
+        double[] result = {0.0 , 0.0};
+        double somX = 0.0;
+        double somY = 0.0;
+        double dF = 0.0;
+        double dL = 0.0;
+        double x = 0.0;
+        double y = 0.0;
+        final double latitudeOffset = 52.15517440;
+        final double longitudeOffset = 5.38720621;
+
+        dF = 0.36*(posToConvert.latitude - latitudeOffset);
+        dL = 0.36*(posToConvert.longitude - longitudeOffset);
+
+        somX = (190094.945 * dL) + (-11832.228 * dF * dL) + (-144.221 * Math.pow(dF, 2) * dL) + (-32.391 * Math.pow(dL, 3))
+                + (-0.705 * dF) + (-2.340 * Math.pow(dF, 3) * dL) + (-0.608 * dF * Math.pow(dL, 3) ) + (-0.008 * Math.pow(dL, 2) ) + (0.148 * Math.pow(dF, 2) * Math.pow(dL, 3));
+
+        somY = (309056.544 * dF) + (3638.893 * Math.pow(dL, 2)) + (73.077 * Math.pow(dF, 2)) + (-157.984 * dF * Math.pow(dL, 2)) + (59.788 * Math.pow(dF, 3) ) + (0.433 * dL)
+                + (-6.439 * Math.pow(dF, 2) * Math.pow(dL, 2)) + (-0.032 * dF * dL) + (0.092 * Math.pow(dL, 4)) + (-0.054 * dF * Math.pow(dL,4));
+
+        x = 155000 + somX;
+        y = 463000 + somY;
+
+        result[0] = x;
+        result[1] = y;
+
+        return result;
+    }
 
 }
